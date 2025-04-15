5412bc5f40031dff146cf07eb6f098404c1aa872
Sora2187 <sora2187@users.noreply.github.com>
2025-04-15 21:03:16 +0530

    Add logic to create grids based on user input

---
+++ script.js
@@ -0,0 +1,20 @@
+const container = document.querySelector('.grid-container');
+const button = document.querySelector('#grid-size');
+button.addEventListener('click', handleClick);
+
+let choice;
+function handleClick() {
+  choice = parseInt(prompt('Enter the size of grid: '));
+  for (let j = 0; j < choice; j++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+
+    for (i = 0; i < choice; i++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      row.appendChild(cell);
+    }
+
+    container.appendChild(row);
+  }
+}
