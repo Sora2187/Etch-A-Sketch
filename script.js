e67ed2fcf06be6d916fe9ae71c5609afc0597710
Vaki Ahmed Khan <Sora2187@users.noreply.github.com>
2025-06-02 01:19:57 +0530

    Add condition for choice
    added an if block so that only take values from 1-64 and throw and alert otherwise

--- script.js
+++ script.js
@@ -8,23 +8,27 @@ resetButton.addEventListener("click", resetClick);
 let choice;
 function handleClick() {
   container.innerHTML = "";
-  choice = parseInt(prompt("Enter the size of grid: "));
-  for (let j = 0; j < choice; j++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
+  choice = parseInt(prompt("Enter the size of grid(range 0-64): "));
+  if (!isNaN(choice) && choice > 0 && choice <= 64) {
+    for (let j = 0; j < choice; j++) {
+      const row = document.createElement("div");
+      row.classList.add("row");
 
-    for (let i = 0; i < choice; i++) {
-      const cell = document.createElement("div");
-      cell.classList.add("cell");
-      row.appendChild(cell);
-      cell.addEventListener("mouseenter", toggleColor);
+      for (let i = 0; i < choice; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        row.appendChild(cell);
+        cell.addEventListener("mouseenter", toggleColor);
 
-      function toggleColor() {
-        cell.classList.add("blk");
+        function toggleColor() {
+          cell.classList.add("blk");
+        }
       }
-    }
 
-    container.appendChild(row);
+      container.appendChild(row);
+    }
+  } else {
+    alert("Please enter a number from 0-64");
   }
 }
 
